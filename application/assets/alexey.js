11a5d6fc88ce98c2572cc5e88ec1b50bc276eef1
Maciej Howański <maciej.howanski@outlook.com>
2021-07-24 15:43:28 +0200

    first css moved

---
+++ application/assets/alexey.js
@@ -0,0 +1,8 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+import './sb_admin_2/css/sb-admin-2.min.css';
+
