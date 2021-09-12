c0fa01ed07c8db67a9a9c9c807f4201415995f74
Maciej Howański <maciej.howanski@outlook.com>
2021-09-12 12:40:27 +0200

    test(Controller): basic DashboardController test

--- application/src/DataFixtures/AppFixtures.php
+++ application/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,18 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $testUser = new User();
+        $testUser->setUsername('test_user');
+        $testUser->setPassword('test_password');
+        $manager->persist($testUser);
 
         $manager->flush();
     }
--- application/tests/Controller/DashboardControllerTest.php
+++ application/tests/Controller/DashboardControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DashboardControllerTest extends WebTestCase
@@ -9,7 +10,17 @@ class DashboardControllerTest extends WebTestCase
     public function testSecurityEnabled(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
         $this->assertResponseRedirects('/login');
+    }
+
+    public function testDashboardOpens()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('test_user');
+        $client->loginUser($testUser);
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
     }
 }
