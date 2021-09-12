c0af98ff4d2db8a10a258d5434baed3436f20ade
Maciej Howański <maciej.howanski@outlook.com>
2021-09-12 20:43:06 +0200

    test: CRAP Reduction

--- application/tests/Entity/NetworkStatisticTest.php
+++ application/tests/Entity/NetworkStatisticTest.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Entity;
 
-use App\Entity\NetworkStatistic;
 use DateTime;
 use PHPUnit\Framework\TestCase;
+use App\Entity\NetworkStatistic;
+use App\Entity\NetworkStatisticTimeFrame;
 
 /**
  * @covers App\Entity\NetworkStatistic
@@ -19,7 +20,7 @@ final class NetworkStatisticTest extends TestCase
         $this->assertEquals(
             expected: DateTime::class,
             actual: get_class($stat->getProbingTime()),
-            message: 'Probing time not set on empty object'
+            message: 'Probing time not set on empty object',
         );
 
         $probingTime = new DateTime('now');
@@ -28,7 +29,69 @@ final class NetworkStatisticTest extends TestCase
         $this->assertEquals(
             expected: $probingTime,
             actual: $stat->getProbingTime(),
-            message: 'Probing time changed after set'
+            message: 'Probing time changed after set',
+        );
+    }
+
+    public function testDataUploadedInFrame(): void
+    {
+        $stat = new NetworkStatistic();
+        $integer = 123;
+        $stat->setDataUploadedInFrame($integer);
+
+        $this->assertEquals(
+            expected: $integer,
+            actual: $stat->getDataUploadedInFrame(),
+            message: 'DataUploadedInFrame changed after set',
+        );
+    }
+
+    public function testDataDownloadedInFrame(): void
+    {
+        $stat = new NetworkStatistic();
+        $integer = 123;
+        $stat->setDataDownloadedInFrame($integer);
+
+        $this->assertEquals(
+            expected: $integer,
+            actual: $stat->getDataDownloadedInFrame(),
+            message: 'DataDownloadedInFrame changed after set',
+        );
+    }
+
+    public function testTimeFrame(): void
+    {
+        $stat = new NetworkStatistic();
+        $timeFrame = new NetworkStatisticTimeFrame();
+        $stat->setTimeFrame($timeFrame);
+
+        $this->assertEquals(
+            expected: $timeFrame,
+            actual: $stat->getTimeFrame(),
+            message: 'TimeFrame changed after set',
+        );
+    }
+
+    public function testReferencePoint(): void
+    {
+        $stat = new NetworkStatistic();
+        $referencePoint = new NetworkStatistic();
+        $stat->setReferencePoint($referencePoint);
+        $this->assertTrue(true);
+    }
+
+    public function testGetTimeLeftTillFrameEnd(): void
+    {
+        $stat = new NetworkStatistic();
+        $timeFrame = new NetworkStatisticTimeFrame();
+        $billingFrameEnd = new \DateTime('tomorrow');
+        $timeFrame->setBillingFrameEnd($billingFrameEnd);
+        $probingTime = new \DateTime('today');
+        $stat->setProbingTime($probingTime);
+        $stat->setTimeFrame($timeFrame);
+        $this->assertEquals(
+            expected: 86400, //day
+            actual: $stat->getTimeLeftTillFrameEnd(),
         );
     }
 }
