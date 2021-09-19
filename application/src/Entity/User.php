419afed7e0a2fc7c2a8df4790213dad46fc5eb24
Maciej Howański <maciej.howanski@outlook.com>
2021-09-19 15:29:35 +0200

    fix: class proxy exception

--- application/src/Entity/User.php
+++ application/src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-final class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
