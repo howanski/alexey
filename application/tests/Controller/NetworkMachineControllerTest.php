29b8d89f936686fdf5cb77ec2a2ba2fe045243ce
Maciej Howański <maciej.howanski@outlook.com>
2021-09-25 06:45:57 +0200

    test: CRAP Reduction

--- application/tests/Controller/NetworkMachineControllerTest.php
+++ application/tests/Controller/NetworkMachineControllerTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
+use App\Entity\NetworkMachine;
+use Doctrine\ORM\EntityManager;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -27,7 +29,7 @@ final class NetworkMachineControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
     }
 
-    public function testNew(): void
+    public function testNew(): int
     {
         $client = static::createClient();
         $client->followRedirects(true);
@@ -53,12 +55,102 @@ final class NetworkMachineControllerTest extends WebTestCase
             haystack: $response,
             message: '---!---> Can\'t find table header after new machine creation.',
         );
+
+        $em = $this->getEntityManager();
+        $machines = $em->getRepository(entityName: NetworkMachine::class)->findAll();
+        $this->assertCount(
+            expectedCount: 1,
+            haystack: $machines,
+            message: 'Was machine persisted ?',
+        );
+        return $machines[0]->getId();
     }
 
-    // private function getEntityManager(): EntityManager
-    // {
-    //     $container = static::getContainer();
-    //     $em = $container->get('doctrine.orm.default_entity_manager');
-    //     return $em;
-    // }
+    /**
+     * @depends testNew
+     */
+    public function testShow(int $machineId): int
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('test_user');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/network/machines/' . $machineId);
+        $this->assertResponseIsSuccessful();
+        return $machineId;
+    }
+
+
+    /**
+     * @depends testShow
+     */
+    public function testEdit(int $machineId): int
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('test_user');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/network/machines/' . $machineId . '/edit');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->selectButton('Update')->form();
+        $form['network_machine']['macAddress']->setValue('AA:BB:CC:DD:FF');
+
+        $client->submit($form);
+
+        $response = $client->getResponse()->getContent();
+
+        $this->assertStringContainsString(
+            needle: '<h6 class="m-0 font-weight-bold text-primary">Machines status</h6>',
+            haystack: $response,
+            message: '---!---> Can\'t find table header after new machine creation.',
+        );
+
+        $em = $this->getEntityManager();
+        $machines = $em->getRepository(entityName: NetworkMachine::class)->findAll();
+        $this->assertCount(
+            expectedCount: 1,
+            haystack: $machines,
+            message: 'Was machine persisted ?',
+        );
+
+        $this->assertEquals(
+            expected: 'AA:BB:CC:DD:FF',
+            actual: $machines[0]->getMacAddress(),
+            message: 'Mac didn\'t saved!',
+        );
+        return $machineId;
+    }
+
+    /**
+     * @depends testEdit
+     */
+    public function testDelete(int $machineId)
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('test_user');
+        $client->loginUser($testUser);
+        $crawler = $client->request('POST', '/network/machines/' . $machineId);
+        $this->assertResponseIsSuccessful();
+
+        $em = $this->getEntityManager();
+        $machines = $em->getRepository(entityName: NetworkMachine::class)->findAll();
+        // FIXME: add token so it will delete
+        $this->assertCount(
+            expectedCount: 1,
+            haystack: $machines,
+            message: 'Was machine deleted ?',
+        );
+    }
+
+    private function getEntityManager(): EntityManager
+    {
+        $container = static::getContainer();
+        $em = $container->get('doctrine.orm.default_entity_manager');
+        return $em;
+    }
 }
