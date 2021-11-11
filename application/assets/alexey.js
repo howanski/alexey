400e45cf746dea8f1b4aa29fecd01177dd486311
Maciej Howański <maciej.howanski@outlook.com>
2021-11-11 07:42:00 +0100

    feat(frontend): WIP: frontend libraries change

--- application/assets/alexey.js
+++ application/assets/alexey.js
@@ -4,9 +4,5 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-import './../node_modules/startbootstrap-sb-admin-2/css/sb-admin-2.css';
 import './../node_modules/@fortawesome/fontawesome-free/css/all.css';
-import './styles/color-overrides-nord.css'
-
-import './../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
-import './../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js';
+// import './styles/color-overrides-nord.css'
