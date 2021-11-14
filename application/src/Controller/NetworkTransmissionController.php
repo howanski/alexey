664c4c506fdcd1713ee29c6a8af477cd9f101f22
Maciej Howański <maciej.howanski@outlook.com>
2021-11-14 23:08:58 +0100

    feat: rewritten transmission style

--- application/src/Controller/NetworkTransmissionController.php
+++ application/src/Controller/NetworkTransmissionController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Class\TransmissionSettings;
-use App\Service\NetworkUsageService;
 use App\Form\TransmissionSettingsType;
 use App\Service\AlexeyTranslator;
 use App\Service\SimpleSettingsService;
@@ -15,30 +14,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\RouterInterface;
 
 #[Route('/network/transmission')]
 class NetworkTransmissionController extends AbstractController
 {
     #[Route('/', name: 'network_transmission')]
-    public function index(
-        SimpleSettingsService $simpleSettingsService,
-        NetworkUsageService $networkUsageService,
-    ): Response {
-        $settings = new TransmissionSettings();
-        $settings->selfConfigure($simpleSettingsService);
-        $transferLeft = $networkUsageService->getLatestStatistic()?->getTransferRateLeft();
-        $current = $transferLeft ? $settings->getProposedThrottleSpeed($transferLeft) : 0;
-        return $this->renderForm('network_transmission/index.html.twig', [
-            'current' => $current,
-        ]);
+    public function index(): Response
+    {
+        return $this->renderForm('network_transmission/index.html.twig');
     }
 
     #[Route('/settings', name: 'network_transmission_settings')]
     public function settings(
         Request $request,
         SimpleSettingsService $simpleSettingsService,
-        NetworkUsageService $networkUsageService,
         AlexeyTranslator $translator,
     ): Response {
         $settings = new TransmissionSettings();
@@ -54,11 +43,8 @@ class NetworkTransmissionController extends AbstractController
             $this->addFlash(type: 'nord14', message: $translator->translateFlash('saved'));
             return $this->redirectToRoute('network_transmission', [], Response::HTTP_SEE_OTHER);
         }
-        $transferLeft = $networkUsageService->getLatestStatistic()?->getTransferRateLeft();
-        $current = $transferLeft ? $settings->getProposedThrottleSpeed($transferLeft) : 0;
         return $this->renderForm('network_transmission/settings.html.twig', [
             'form' => $form,
-            'current' => $current,
         ]);
     }
 
