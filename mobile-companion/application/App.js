f61554ebc876718a4c5a4fe48b8b4b061e4d68b4
Maciej Howański <maciej.howanski@outlook.com>
2021-12-28 20:41:56 +0100

    feat(mobile): WIP: initial qr-based login

--- mobile-companion/application/App.js
+++ mobile-companion/application/App.js
@@ -1,44 +1,72 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
-    const [serverUri, setServerUri] = useState("https://alexey_dev");
-    const [serverSecret, setServerSecret] = useState("TotallySecret!");
-    const [mainResult, setMainResult] = useState("");
+    const [hasPermission, setHasPermission] = useState(null);
+    const [serverUri, setServerUri] = useState("");
+    const [serverSecret, setServerSecret] = useState("");
+    const [defaultPath, setDefaultPath] = useState("");
+    const [serverResponseCode, setServerResponseCode] = useState(401);
+    const [serverResponseMessage, setServerResponseMessage] = useState("XYZ");
+    const [freezeScanner, setFreezeScanner] = useState(false);
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        if (!freezeScanner) {
+            let obj = JSON.parse(data);
+            setServerUri(obj.server);
+            setServerSecret(obj.token);
+            setDefaultPath(obj.path);
+            let fullPath = serverUri + defaultPath;
+            console.log(fullPath);
+            if (fullPath) {
+                setFreezeScanner(true);
+                fetch(fullPath, {
+                    method: "GET",
+                    headers: { "X-ALEXEY-SECRET": serverSecret },
+                })
+                    .then((response) => response.json())
+                    .then((jsonResponse) => {
+                        setServerResponseCode(jsonResponse.code);
+                        setServerResponseMessage(jsonResponse.message);
+
+                        // setServerResponseObject(jsonResponse);
+                        console.log(jsonResponse);
+                        setFreezeScanner(false);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } else {
+                setFreezeScanner(false);
+            }
+        }
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Server URI:</Text>
-            <TextInput
-                style={styles.input}
-                defaultValue={serverUri}
-                onChangeText={(text) => setServerUri(text)}
-            />
-            <Text style={styles.text}>Server Secret:</Text>
-            <TextInput
-                style={styles.input}
-                defaultValue={serverSecret}
-                onChangeText={(text) => setServerSecret(text)}
-            />
-            <Button
-                onPress={() => {
-                    setMainResult("--- 😅 ---");
-                    fetch(serverUri + "/api/dashboard", {
-                        method: "GET",
-                        headers: { "X-ALEXEY-SECRET": serverSecret },
-                    })
-                        .then((response) => response.json())
-                        .then((json) => {
-                            setMainResult(json.code + " : " + json.message);
-                        })
-                        .catch((error) => {
-                            console.error(error);
-                        });
-                }}
-                title={"Connect"}
-            />
-            <Text style={styles.text}>{mainResult}</Text>
-            <StatusBar style="auto" />
+            {serverResponseCode == 401 && (
+                <BarCodeScanner
+                    onBarCodeScanned={handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+            )}
+            {!(serverResponseCode === 401) && (
+                <Text style={styles.text}>{serverResponseMessage}</Text>
+            )}
         </View>
     );
 }
@@ -51,11 +79,11 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     text: {
-        color: "#d8dee9",
+        color: "#cf0a3b",
         fontSize: 42,
     },
     input: {
-        color: "#d8dee9",
+        color: "#cf0a3b",
         height: 40,
         borderColor: "gray",
         borderWidth: 1,
