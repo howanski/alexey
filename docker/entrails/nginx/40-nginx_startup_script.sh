6460d8538547fa27c9e02342788ee5c0889ffa08
Maciej Howański <howanski@proton.me>
2026-01-23 05:46:37 +0100

    build(deps): update ngrok fetch url

--- docker/entrails/nginx/40-nginx_startup_script.sh
+++ docker/entrails/nginx/40-nginx_startup_script.sh
@@ -3,7 +3,7 @@ cd /ngrok
 export $(cat .env.local | grep -v '#' | awk '/=/ {print $1}')
 
 if [ ! -f ngrok ]; then
-    wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+    wget https://bin.ngrok.com/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
     tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 fi
 rm -f ngrok-v3-stable-linux-amd64.tgz
