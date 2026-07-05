e4342ca0a910b8ee2b40a12b452a90c2ff533a3e
Maciej Howański <howanski@proton.me>
2026-07-05 15:49:22 +0200

    feat: support static url in ngrok via .env.local

--- docker/entrails/nginx/ngrok/ngrok_tunnel.sh
+++ docker/entrails/nginx/ngrok/ngrok_tunnel.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 cd /ngrok
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf && echo "nameserver 9.9.9.9" >> /etc/resolv.conf
-./ngrok http 443 > /dev/null
+if [ -n "$NGROK_URL" ]; then
+    ./ngrok http --url=$NGROK_URL 443 > /dev/null
+else
+    ./ngrok http 443 > /dev/null
+fi
 sleep 30 #sleep on tunnel break
